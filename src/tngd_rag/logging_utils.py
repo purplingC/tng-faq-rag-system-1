57b437168ee0fd1ecadb625eb1857e52ee5fe393
purplingC <samyuzhen@gmail.com>
2026-09-15 23:38:22 +0800

    Added logging_utils.py

---
+++ src/tngd_rag/logging_utils.py
@@ -0,0 +1,21 @@
+"""Logging setup. """
+
+from __future__ import annotations
+import logging
+import sys
+
+LOG = logging.getLogger("tngd_rag")
+
+
+def setup_logging(verbose: bool = False) -> None:
+    level = logging.DEBUG if verbose else logging.INFO
+    handler = logging.StreamHandler(sys.stderr)
+    handler.setFormatter(
+        logging.Formatter(
+            "%(asctime)s %(levelname)-7s %(name)s | %(message)s",
+            datefmt="%Y-%m-%d %H:%M:%S",
+        )
+    )
+    LOG.handlers[:] = [handler]
+    LOG.setLevel(level)
+    LOG.propagate = False
